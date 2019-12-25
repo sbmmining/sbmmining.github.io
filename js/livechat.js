9472bca68360c7a7c27c6cd6fb184baeae0e9c5c
sbmmining <59195362+sbmmining@users.noreply.github.com>
2019-12-25 16:05:39 +0800

    Add files via upload

---
+++ js/livechat.js
@@ -0,0 +1,15 @@
+//#@+livechat-sbm-content
+document.write('<script language="javascript" src="https://mnn.zoosnet.net/JS/LsJS.aspx?siteid=MNN94995150&float=1&lng=en"></script>');
+//#@-livechat-sbm-content
+
+document.oncontextmenu=new Function("event.returnValue=false");
+
+document.onselectstart=new Function("event.returnValue=false");
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-55571935-175', 'auto');
+  ga('send', 'pageview');
